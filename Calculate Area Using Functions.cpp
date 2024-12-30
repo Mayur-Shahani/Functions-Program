a266b5c13e63d45ba5357274d115c47a9d52d0f0
mayur-shahani <shahanimkwork@gmail.com>
2024-12-30 23:45:06 +0500

    done

--- Calculate Area Using Functions.cpp
+++ Calculate Area Using Functions.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int calculateArea(int length, int width);
+int calArea(int length, int width);
 
 int main() {
     int length, width;
@@ -12,12 +12,12 @@ int main() {
     cout << "Enter the width of the rectangle: ";
     cin >> width;
 
-    int area = calculateArea(length, width);
+    int area = calArea(length, width);
     cout << "The area of the rectangle is: " << area << endl;
     return 0;
 }
 
-int calculateArea(int length, int width) {
+int calArea(int length, int width) {
     return length * width;
 }
 
